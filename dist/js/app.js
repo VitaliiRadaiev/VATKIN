3ce448c2487a8358bba9dd98a57d2a8e6414643f
Spacespire <aleksandra@spacespire.com>
2022-09-19 18:40:23 +0300

    fix

--- dist/js/app.js
+++ dist/js/app.js
@@ -1429,7 +1429,24 @@ window.popup = {
 		{
     let aboutHero = document.querySelector('[data-about-hero]');
     if(aboutHero) {
-        
+        let anchor = aboutHero.querySelector('.about-hero__anchor');
+        let bgItems = aboutHero.querySelectorAll('.about-hero__bg-item');
+
+        aboutHero.addEventListener('mousemove', (e) => {
+            anchor.hidden = true;
+            let hoverItem = document.elementFromPoint(e.clientX, e.clientY).closest('.about-hero__bg-item');
+            anchor.hidden = false;
+
+            if(hoverItem) {
+                if(bgItems.length) {
+                    hoverItem.classList.add('hover');
+                    bgItems.forEach(i => {
+                        if(i === hoverItem) return;
+                        i.classList.remove('hover');
+                    })
+                }
+            }
+        })
     }
 };
 	}
